6d248eed73505b2757fa1059f9689deaa512f381
Mike Almond <mike.almond@madebypi.co.uk>
2014-08-08 11:13:33 +0100

    Add toggles for follow behavior on x/y axis separately Make easing configurable

--- src/flambe/camera/behaviours/SpriteFollowBehaviour.hx
+++ src/flambe/camera/behaviours/SpriteFollowBehaviour.hx
@@ -9,34 +9,50 @@ import flambe.display.Sprite;
  */
 @:final class SpriteFollowBehaviour extends CameraControlBehaviour {
 	
-	var resetX			:Float = 0;
-	var resetY			:Float = 0;
+	var resetX									:Float = 0;
+	var resetY									:Float = 0;
+	var resetRotation							:Float = 0;
 	
-	var _target			:Entity = null;	
-	var _targetSprite	:Sprite = null;	
-	
-	public var xEaseTime:Float = .25;
-	public var yEaseTime:Float = .25;
+	var _target									:Entity = null;	
+	var _targetSprite							:Sprite = null;	
 	
 	public var target			(get, set)		:Entity;	
 	public var targetSprite		(get, never)	:Sprite;
-	
 	public var canFollowTarget	(get, never)	:Bool;
+	
+	public var xEaseTime						:Float = .25;
+	public var yEaseTime						:Float = .25;
+	public var rotationEaseTime					:Float = 0;
+	
+	public var easeX							:Float->Float;
+	public var easeY							:Float->Float;
+	public var easeRotation						:Float->Float;
+	
+	public var followTargetX					:Bool = true;
+	public var followTargetY					:Bool = true;
 	public var followTargetRotation				:Bool = false;
 	
 	
 	public function new(camera:Camera, xEaseTime:Float=.25, yEaseTime:Float=.25) {
 		super(camera);
-		this.xEaseTime = xEaseTime;
-		this.yEaseTime = yEaseTime;
+		
+		this.xEaseTime 	= xEaseTime;
+		this.yEaseTime 	= yEaseTime;
+		
+		followTargetX 	= xEaseTime >= 0;
+		followTargetY 	= yEaseTime >= 0;
+		
+		easeX 			= Ease.quadInOut;
+		easeY 			= Ease.quadInOut;
+		easeRotation	= Ease.quadInOut;
 	}
 	
 	
 	override public function update(dt:Float) {
 		if (enabled && canFollowTarget) {
-			camera.controller.targetX.animateTo(targetSprite.x._, xEaseTime);
-			camera.controller.targetY.animateTo(targetSprite.y._, yEaseTime);
-			if (followTargetRotation) camera.controller.rotation._ = -targetSprite.rotation._;
+			if (followTargetX) camera.controller.targetX.animateTo(targetSprite.x._, xEaseTime, easeX);
+			if (followTargetY) camera.controller.targetY.animateTo(targetSprite.y._, yEaseTime, easeY);
+			if (followTargetRotation) camera.controller.rotation.animateTo(-targetSprite.rotation._, rotationEaseTime, easeRotation);
 		}
 	}
 	
@@ -50,11 +66,13 @@ import flambe.display.Sprite;
 	
 	override function set_enabled(value:Bool) {
 		if (_enabled && !value) {
-			camera.controller.targetX.animateTo(resetX, xEaseTime, Ease.quadInOut);
-			camera.controller.targetY.animateTo(resetY, yEaseTime, Ease.quadInOut);
+			if (followTargetX) camera.controller.targetX.animateTo(resetX, xEaseTime, easeX);
+			if (followTargetY) camera.controller.targetY.animateTo(resetY, yEaseTime, easeY);
+			if (followTargetRotation) camera.controller.rotation.animateTo(resetRotation, rotationEaseTime, easeRotation);
 		} else if (!_enabled && value) {
 			resetX = camera.targetX;
 			resetY = camera.targetY;
+			resetRotation = camera.rotation;
 		}
 		return _enabled = value;
 	}
